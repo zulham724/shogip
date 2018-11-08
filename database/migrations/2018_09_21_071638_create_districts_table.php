0c52b6aababacdb0adbcc36248dee2aad6256e21
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2018-11-08 09:37:40 +0700

    update landing page

--- database/migrations/2018_09_21_071638_create_districts_table.php
+++ database/migrations/2018_09_21_071638_create_districts_table.php
@@ -14,6 +14,7 @@ class CreateDistrictsTable extends Migration
     public function up()
     {
         Schema::create('districts', function (Blueprint $table) {
+            $table->engine = "InnoDB";
             $table->increments('id');
             $table->integer('city_id')->unsigned();
             $table->string('name');
