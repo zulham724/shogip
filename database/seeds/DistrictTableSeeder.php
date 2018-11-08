1972856d36bdbe4154ebdf7467efd6b2f2c39f37
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2018-11-08 09:32:52 +0700

    update landing page

--- database/seeds/DistrictTableSeeder.php
+++ database/seeds/DistrictTableSeeder.php
@@ -13,17 +13,16 @@ class DistrictTableSeeder extends Seeder
     public function run()
     {
    		$file = fopen(database_path('csv/districts.csv'),"r");
-            $data = array();
-            while (($row = fgetcsv($file, 0, ",")) !== FALSE) {
-                $data[] = $row;
-            }
-            foreach ($data as $d) {
-                $s = new District();
-                $s->id = $d[0];
-                $s->city_id = $d[1];
-                $s->name = $d[3];
-                $s->save();
-            }
-        }		
+        $data = array();
+        while (($row = fgetcsv($file, 0, ",")) !== FALSE) {
+            $data[] = $row;
+        }
+        foreach ($data as $d) {
+            $s = new District();
+            $s->id = $d[0];
+            $s->city_id = $d[1];
+            $s->name = $d[3];
+            $s->save();
+        }	
     }
 }
