a6a2230d83898315dcb0a8f604ee47cd3894730e
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2018-11-23 10:25:51 +0700

    update password confirmation

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -36,34 +36,34 @@
 						<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="type something" required> 
 
 						@if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
 					</div>
 					<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 						<label>Email</label>
 						<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="type something" required>
 
 						@if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif 
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif 
 					</div> 
 					<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 						<label>Password</label>
 						<input id="password" type="password" class="form-control" name="password" placeholder="type something" required> 
 
 						@if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
                         @endif
 					</div> 
 					<div class="form-group">
 						<label>Confirm Password</label>
-						<input id="pasword-confirm" type="password" class="form-control" name="pasword-confirm" placeholder="type something" required>
+						<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 					</div>			
 					
 					<button type="submit" class="btn btn-success pull-right"><i class="fa fa-check"></i> Submit</button>
